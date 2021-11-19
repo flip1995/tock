c75b250a9ac01b92f85b32dddc3e388f2764e886
Brad Campbell <bradjc5@gmail.com>
2021-11-19 12:38:53 -0500

    kernel: process: add comments

--- kernel/src/process.rs
+++ kernel/src/process.rs
@@ -421,6 +421,13 @@ pub trait Process {
     /// and cannot be edited by the process.
     fn flash_non_protected_start(&self) -> *const u8;
 
+    /// Return the permissions for this process for a given `driver_num`.
+    ///
+    /// The returned `CommandPermissions` will indicate if any permissions for
+    /// individual command numbers are specified. If there are permissions set
+    /// they are returned as a 64 bit bitmask for sequential command numbers. To
+    /// request permissions for a starting command number other than 0, an
+    /// optional `offset` can be provided.
     fn get_command_permissions(
         &self,
         driver_num: usize,
